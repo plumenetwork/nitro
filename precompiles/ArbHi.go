bf1d204b3d8c7f640dde60521a661fdf5bfb87f0
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2024-01-27 21:06:34 -0500

    add Hi event to ArbHi precompile

--- precompiles/ArbHi.go
+++ precompiles/ArbHi.go
@@ -2,9 +2,14 @@ package precompiles
 
 // ArbHi provides a friendly greeting to anyone who calls it.
 type ArbHi struct {
-	Address addr // 0x11a
+	Address   addr // 0x11a
+
+	// Custom events
+	Hi        func(ctx, mech, addr) error
+	HiGasCost func(addr) (uint64, error)
 }
 
 func (con *ArbHi) SayHi(c ctx, evm mech) (string, error) {
-	return "hi", nil
+	err := con.Hi(c, evm, c.caller)
+	return "hi", err
 }
